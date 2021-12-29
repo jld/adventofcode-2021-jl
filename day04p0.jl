3120dc1e4631c93b71c6d37d79dc5428587d270f
Jed Davis <jld@xlerb.net>
2021-12-28 21:04:48 -0700

    Day 4 part 1: in which I learn about the array methods of the collection functions.

--- day04p0.jl
+++ day04p0.jl
@@ -18,3 +18,33 @@ function read_game(f)
    b = read_boards(f)
    Game(d, b)
 end
+
+function do_draws(g::Game, n)
+   marked = falses(size(g.board))
+   for draw = g.draws[begin:n]
+      @. marked |= g.board == draw
+   end
+   marked
+end
+
+function score_board(board::Matrix{Int}, marked::BitMatrix, draw::Int)
+   sum(@. board * !marked) * draw
+end
+
+function play_bingo(g::Game)
+   marked = falses(size(g.board))
+   for draw = g.draws
+      @. marked |= g.board == draw
+      hwins = vec(reduce(|, reduce(&, marked, dims=1), dims=2))
+      vwins = vec(reduce(|, reduce(&, marked, dims=2), dims=1))
+      win = findfirst(identity, [hwins vwins])
+      if win != nothing
+         win = win[1]
+         return score_board(g.board[:, :, win], marked[:, :, win], draw)
+      end
+   end
+   @assert(false)
+   nothing
+end
+
+bingo_file(path) = play_bingo(open(read_game, path))
